a2fcc4a98339e9d64d39b765dd3529828741f717
Yusuke Hatanaka <hatanaka@vanilab.jp>
2012-10-13 06:48:22 +0900

    forgot it

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+;; kill buffer
+(define-key global-map (kbd "C-x C-k") 'kill-buffer)
+
 ;; default encoding
 (set-language-environment 'utf-8)
 (prefer-coding-system 'utf-8)
@@ -53,6 +56,11 @@
   (auto-install-update-emacswiki-package-name t)
   (auto-install-compatibility-setup))
 
+;; key-chord
+(when (require 'key-chord nil t)
+  (key-chord-mode 1)
+  (key-chord-define-global "df" 'describe-bindings))
+
 ;; cua-mode
 (cua-mode t)
 (setq cua-enable-cua-keys nil)
@@ -103,53 +111,53 @@
   (when (require 'descbinds-anything nil t)
     (descbinds-anything-install))
   
-  (define-key global-map (kbd "C-l") 'anything))
+  (define-key global-map (kbd "C-l") 'anything)
 
-;; anything-gtags
-(when (require 'anything-gtags nil t)
-  (setq anything-for-tags
-	(list anything-c-source-gtags-select))
+  ;; anything-gtags
+  (when (require 'anything-gtags nil t)
+    (setq anything-for-tags
+	  (list anything-c-source-gtags-select))
 
-  (defun anything-for-tags ()
-    "Preconfigured `anything' for anything-for-tags."
+    (defun anything-for-tags ()
+      "Preconfigured `anything' for anything-for-tags."
+      (interactive)
+      (anything anything-for-tags
+		(thing-at-point 'symbol)
+		nil nil nil "*anything for tags*"))
+
+    (define-key global-map (kbd "M-t") 'anything-for-tags)
+    (define-key global-map (kbd "C-M-t") 'gtags-find-rtag))
+
+  ;; anything-show-kill-ring
+  (define-key global-map (kbd "M-y") 'anything-show-kill-ring)
+
+  ;; anything-c-moccur
+  (when (require 'anything-c-moccur nil t)
+    (setq
+     anything-c-moccur-anything-idle-delay 0.1
+     anything-c-moccur-highlight-info-line-flag t
+     anything-c-moccur-enable-auto-look-flag t
+     anything-c-moccur-enable-initial-pattern t)
+    (global-set-key (kbd "C-M-o") 'anything-c-moccur-occur-by-moccur))
+
+  ;; anything-for-document
+  (setq anything-for-document-sources
+	(list anything-c-source-man-pages
+	      anything-c-source-info-cl
+	      anything-c-source-info-pages
+	      anything-c-source-info-elisp
+	      anything-c-source-apropos-emacs-commands
+	      anything-c-source-apropos-emacs-functions
+	      anything-c-source-apropos-emacs-variables))
+
+  (defun anything-for-document ()
+    "Preconfigured `anything' for anything-for-document."
     (interactive)
-    (anything anything-for-tags
-	      (thing-at-point 'symbol)
-	      nil nil nil "*anything for tags*"))
-
-  (define-key global-map (kbd "M-t") 'anything-for-tags)
-  (define-key global-map (kbd "C-M-t") 'gtags-find-rtag))
-
-;; anything-show-kill-ring
-(define-key global-map (kbd "M-y") 'anything-show-kill-ring)
-
-;; anything-c-moccur
-(when (require 'anything-c-moccur nil t)
-  (setq
-   anything-c-moccur-anything-idle-delay 0.1
-   anything-c-moccur-highlight-info-line-flag t
-   anything-c-moccur-enable-auto-look-flag t
-   anything-c-moccur-enable-initial-pattern t)
-  (global-set-key (kbd "C-M-o") 'anything-c-moccur-occur-by-moccur))
-
-;; anything-for-document
-(setq anything-for-document-sources
-      (list anything-c-source-man-pages
-	    anything-c-source-info-cl
-	    anything-c-source-info-pages
-	    anything-c-source-info-elisp
-	    anything-c-source-apropos-emacs-commands
-	    anything-c-source-apropos-emacs-functions
-	    anything-c-source-apropos-emacs-variables))
-
-(defun anything-for-document ()
-  "Preconfigured `anything' for anything-for-document."
-  (interactive)
-  (anything anything-for-document-sources
-	    (thing-at-point 'symbol) nil nil nil
-	    "*anything for document*"))
-(define-key global-map (kbd "C-M-d") 'anything-for-document)
-
+    (anything anything-for-document-sources
+	      (thing-at-point 'symbol) nil nil nil
+	      "*anything for document*"))
+  (define-key global-map (kbd "C-M-d") 'anything-for-document))
+  
 ;; package
 (when (require 'package nil t)
   (add-to-list 'package-archives
@@ -208,11 +216,3 @@
   (if window-system
       (define-key elscreen-map (kbd "C-z") 'iconify-or-deiconify-frame)
     (define-key elscreen-map (kbd "C-z") 'suspend-emacs)))
-
-
-
-
-
-
-
-
